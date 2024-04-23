1be11ea60a03b3e578cd9350b5c0351448ec3136
WR Smiley <smileywestin@gmail.com>
2024-04-23 07:11:09 -0600

    Removes country.ts, removes references to country.ts

--- src/app/world-map/country.ts
+++
@@ -1,7 +0,0 @@
-export interface Country {
-    countryName: string;
-    capital: string;
-    region: string;
-    income: string;
-    
-}
--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Country } from './country';
+import { Component } from '@angular/core';
 import { ApiintegrationService } from './apiintegration.service';
 
 
